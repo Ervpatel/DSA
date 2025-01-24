96f0e21331cfed3f485d84f5581e84b8749e9016
Ervpatel <ervpatel1010>
2025-01-24 19:55:27 +0530

    changing another time

--- practice.cpp
+++ practice.cpp
@@ -1,18 +1,20 @@
 #include<iostream>
-using namespace std;
+using  namespace std;
 
 int main() {
 
-    int n;
-    cout << "Enter the numbers of raw :";
-    cin >> n;
+  int n;
+  cout << "Enter the value of n :";
+  cin >> n;
+  int k = 1;
 
-    for(int i = 1 ; i <= n ; i++) {
-        for(int j = 1 ; j <= i ; j++) {
-            cout << "* ";
-        }
-        cout << endl;
+  for(int i = 1 ; i <= n ; i++) {
+    for(int j = 1; j <= i ; j++) {
+      cout << i;
     }
+    cout << endl;
+  }
 
-    return 0;
+
+  return 0;
 }
